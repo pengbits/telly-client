5f90a6e7e0608dd75768bf9936f534c350683c8e
Dave Paul <david.paul@showtime.net>
2016-12-13 15:08:22 -0500

    state sketches

---
+++ state.sketches.js
@@ -0,0 +1,40 @@
+SET_SEARCH
+{
+  search: "Dicte",
+  isFetching: false
+}
+
+REQUEST_SEARCH
+{
+  search: "Dicte",
+  isFetching: true
+}
+
+RECEIVE_SEARCH
+{
+  search: "Dicte",
+  isFetching: false,
+  results: [
+    {
+      "aliases": [],
+      "banner": "graphical/265571-g.jpg",
+      "firstAired": "2013-01-07",
+      "id": 265571,
+      "network": "TV 2",
+      "overview": "Dicte is a dedicated reporter and refuses to give up before she has her story. Her stubborness gives her problems immediately with the policeman John Wagner, and they often get into clashes with each other.",
+      "seriesName": "Dicte",
+      "status": "Continuing"
+    }
+  ]
+}
+
+SET_SHOWS
+{
+  shows: ["Dicte","Berlin Station","Chewing Gum"],
+  isFetching: false
+}
+
+SELECT_SHOW
+{
+  show: "Dicte"
+}

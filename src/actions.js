f4747300e3e088c9204015acfaa89da28a469e14
Dave Paul <david.paul@showtime.net>
2016-12-14 14:53:36 -0500

    stopped dead cos of CORS/same-domain-access-policy

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,6 @@
 import fetch from 'isomorphic-fetch'
 
+
 // load initial shows into list
 export const SET_SHOWS = 'SET_SHOWS'
 export function setShows(shows) {
@@ -49,6 +50,8 @@ export function receiveSearch(searchTerm, json) {
   }
 }
 
+// only good for 24 hours
+export const API_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0ODE4Mjk2MDAsImlkIjoiaGVsbG93b3JsZGFwcCIsIm9yaWdfaWF0IjoxNDgxNzQzMjAwLCJ1c2VyaWQiOjQ2OTgzOSwidXNlcm5hbWUiOiJteWRyb25lIn0.zNEPjXV31Jitg-dv5bDgWf5bcjDVzA8VjYlfrxTzUSwoamiXdvxR7Eo8QX5pOyWff7K7ATouBdXNBTiQL1sYmhFmQBX51WWRJU5mqXmGoGQOUnOPaZ4MvNbR1cUCGyIqqoDISzoHpbTnR5iNgnFJDpNDOMgC4pL3a-kwgKe_FelixAIFS8CL9aZH7057HDgyFqr8XzfzLqHvYbTqYzSOpnBylTCx6ywg8eJGgjdtYheZRYvY3hcfWAWnESc0sInIoh4Wuc1zzRV2w6LkS_OxhgNfXvg51mbblgJYK0ZR61CdlZfiVEKGAe17anARKX64QBL72YlxEFQbD4COc90k0g";
 export const fetchSearch = () =>{
   
   // Thunk middleware knows how to handle functions.
@@ -60,21 +63,28 @@ export const fetchSearch = () =>{
     let searchTerm = getState().search.term;
     dispatch(requestSearch(searchTerm));
     
-    // simulate latency
-    setTimeout(() => {
-      dispatch(receiveSearch(searchTerm, {
-        "data": [{
-          "aliases": [],
-          "banner": "graphical/265571-g.jpg",
-          "firstAired": "2013-01-07",
-          "id": 265571,
-          "network": "TV 2",
-          "overview": "Dicte is a dedicated reporter and refuses to give up before she has her story. Her stubborness gives her problems immediately with the policeman John Wagner, and they often get into clashes with each other.",
-          "seriesName": "Dicte",
-          "status": "Continuing"
-        }]
-      }))
-    }, 1000)
+    // The function called by the thunk middleware can return a value,
+   // that is passed on as the return value of the dispatch method.
+   // In this case, we return a promise to wait for.
+   // This is not required by thunk middleware, but it is convenient for us.
+
+  return fetch(`https://api.thetvdb.com/search/series?name=${searchTerm}`, {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${API_TOKEN}`
+    }
+  })
+     .then(response => response.json())
+     .then(json => {
+       console.log(json)
+       dispatch(receiveSearch(searchTerm, json))
+     })
+     .catch(error => {
+       console.log(error)
+     })
   }
 }
 

9622fa4162644a6d6b44bae90d47581c63a40aa3
Dave Paul <david.paul@showtime.net>
2017-02-14 16:41:28 -0500

    whitespace

--- src/actions/show.js
+++ src/actions/show.js
@@ -40,7 +40,6 @@ export const createShow = (show) => {
   }
 }
 
-
 export const updateShow = (show) => {
   return (dispatch, getState) => {
     return dispatch({
--- src/actions/shows.js
+++ src/actions/shows.js
@@ -14,7 +14,6 @@ export const getShows = () => {
   }
 }
 
-
 export const fetchShows = () => {
   return (dispatch, getState) => {
     dispatch({

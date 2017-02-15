016343346f8bb5cf3046c8424f8298fd636d38c4
Dave Paul <david.paul@showtime.net>
2017-02-15 13:37:52 -0500

    shows, networks tabs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,19 @@ const Nav = (props) => {
   return (
     <nav className="navbar navbar-default navbar-fixed-top">
       <div className="container">
+        <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="#/shows">Telly</a>
+      </div>
        <div id="navbar" className="navbar-collapse collapse">
          <ul className="nav navbar-nav">
-           <li className="active"><a href="#">Home</a></li>
-           <li><a href="#about">About</a></li>
-           <li><a href="#contact">Contact</a></li>
+           <li className="active"><a href="#/shows">Shows</a></li>
+           <li><a href="#/networks">Networks</a></li>
          </ul>
          <ul className="nav navbar-nav navbar-right">
            <li><a href="../navbar/">Default</a></li>
